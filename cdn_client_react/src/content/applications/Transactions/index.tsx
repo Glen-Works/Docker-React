b13bcd291f5193a713a712c1abb5fcd4b8078407
GlenSHIHSHIH <a0935745150@gmail.com>
2022-10-17 15:58:03 +0800

    add mui dataTable to Transaction table

--- cdn_client_react/src/content/applications/Transactions/index.tsx
+++ cdn_client_react/src/content/applications/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Container, Grid } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useNavigate } from 'react-router-dom';
@@ -7,6 +8,9 @@ import PageTitleWrapper from 'src/components/PageTitleWrapper';
 import { useAuthStateContext } from 'src/contexts/AuthContext';
 import PageHeader from './PageHeader';
 
+import MUIDataTable from "mui-datatables";
+
+import createCache from "@emotion/cache";
 import RecentOrders from './RecentOrders';
 
 function ApplicationsTransactions() {
@@ -16,6 +20,58 @@ function ApplicationsTransactions() {
   useEffect(() => {
 
   }, []);
+
+  const columns = [
+    {
+      name: "name",
+      label: "Name",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "company",
+      label: "Company",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "city",
+      label: "City",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "state",
+      label: "State",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+  ];
+
+  const data = [
+    { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+    { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+    { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+    { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
+  ];
+
+  const options = {
+    filterType: 'checkbox',
+    sort: true,
+  };
+
+  const muiCache = createCache({
+    "key": "mui",
+    "prepend": true
+  });
 
   return (
     <>
@@ -34,6 +90,16 @@ function ApplicationsTransactions() {
           spacing={3}
         >
           <Grid item xs={12}>
+            {/* <CacheProvider value={muiCache}> */}
+            <ThemeProvider theme={createTheme()}>
+              <MUIDataTable
+                title={"Employee List"}
+                data={data}
+                columns={columns}
+                options={options}
+              />
+            </ThemeProvider>
+            {/* </CacheProvider> */}
             <RecentOrders />
           </Grid>
         </Grid>
