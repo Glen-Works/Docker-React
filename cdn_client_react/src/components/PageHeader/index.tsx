0fc542c264100de856f5255b7b063d09bf6c1845
GlenSHIHSHIH <a0935745150@gmail.com>
2022-12-13 14:33:24 +0800

    update fontSize

--- cdn_client_react/src/components/PageHeader/index.tsx
+++ cdn_client_react/src/components/PageHeader/index.tsx
@@ -11,7 +11,7 @@ function PageHeader(props: PageHeaderProp) {
   return (
     <Grid container justifyContent="space-between" alignItems="center">
       <Grid item>
-        <Typography variant="h3" component="h3" gutterBottom>
+        <Typography variant="h4" component="h4" gutterBottom>
           {title}
         </Typography>
         <Typography variant="subtitle2">
